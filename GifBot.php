ef996201f7e5fd9bac1c27d6f12f745212df9f1a
andy matthews <andy@commadelimited.com>
2014-05-09 10:19:42 -0500

    Add simple demo page, "no image" response
    This PR adds a simple demo page which allows users to test GifBot on
    their server before adding it to Slack. It also adds a "no image"
    response for times when the given search string returns nothing from
    Giphy.

--- GifBot.php
+++ GifBot.php
@@ -7,8 +7,12 @@ define('GIPHY_API_KEY', 'dc6zaTOxFJmzC');
 
 // Get request
 $trigger = $_POST['trigger_word'];
-$search  = trim(substr($_POST['text'], strlen($trigger) + 1));
+$text = $_POST['text'];
 
+// build search string
+$search  = trim(substr($text, strlen($trigger) + 1));
+
+// fail if search string is empty
 if ($search == '') {
     echo 'OK';
     exit;
@@ -16,16 +20,24 @@ if ($search == '') {
 
 // Query Giphy - http://giphy.com/
 $limit    = 25;
-$response = file_get_contents('http://api.giphy.com/v1/gifs/search?q=' . urlencode($search) . '&api_key=' . GIPHY_API_KEY . '&limit=' . $limit . '&offset=0');
+$response = file_get_contents('http://api.giphy.com/v1/gifs/search?q=' .
+            urlencode($search) . '&api_key=' . GIPHY_API_KEY . '&limit=' . $limit . '&offset=0');
 $response = json_decode($response);
 
 // Pick a random GIF
 $gifs  = $response->data;
 $count = count($gifs);
-$image = $gifs[rand(0, $count - 1)]->images->original;
 
-// Respond with GIF
+// make sure we have an image
+if ($count) {
+    $image = $gifs[rand(0, $count - 1)]->images->original;
+    $response = $image->url;
+} else {
+    $response = 'No image found for `' . $search . '`';
+}
+
+// Respond
 header('Content-Type: application/json');
 echo json_encode(array(
-    'text' => $image->url
+    'text' => $response
 ));
