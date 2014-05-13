013ca74a09067ef7666e62eda691375301ce7d49
Garrett St. John <garrett@hellobold.com>
2014-05-13 08:20:16 -0700

    Moving to fixed width GIF to be a bit more bandwidth friendly

--- GifBot.php
+++ GifBot.php
@@ -7,12 +7,12 @@ define('GIPHY_API_KEY', 'dc6zaTOxFJmzC');
 
 // Get request
 $trigger = $_POST['trigger_word'];
-$text = $_POST['text'];
+$text    = $_POST['text'];
 
-// build search string
-$search  = trim(substr($text, strlen($trigger) + 1));
+// Build search string
+$search = trim(substr($text, strlen($trigger) + 1));
 
-// fail if search string is empty
+// Fail if search string is empty
 if ($search == '') {
     echo 'OK';
     exit;
@@ -21,16 +21,16 @@ if ($search == '') {
 // Query Giphy - http://giphy.com/
 $limit    = 25;
 $response = file_get_contents('http://api.giphy.com/v1/gifs/search?q=' .
-            urlencode($search) . '&api_key=' . GIPHY_API_KEY . '&limit=' . $limit . '&offset=0');
+    urlencode($search) . '&api_key=' . GIPHY_API_KEY . '&limit=' . $limit . '&offset=0');
 $response = json_decode($response);
 
 // Pick a random GIF
 $gifs  = $response->data;
 $count = count($gifs);
 
-// make sure we have an image
+// Make sure we have an image
 if ($count) {
-    $image = $gifs[rand(0, $count - 1)]->images->original;
+    $image    = $gifs[rand(0, $count - 1)]->images->fixed_width_downsampled;
     $response = $image->url;
 } else {
     $response = 'No image found for `' . $search . '`';
@@ -41,3 +41,7 @@ header('Content-Type: application/json');
 echo json_encode(array(
     'text' => $response
 ));
+
+exit;
+
+function
